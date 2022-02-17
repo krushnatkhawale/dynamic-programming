af1569853e319d34d2da639698e79534a68ace82
unknown <krushnatkhawale@gmail.com>
2022-02-17 23:54:12 +0530

    Rectoring: unnecessary parameters removed

--- java/src/main/java/my/practice/FibonacciSeries.java
+++ java/src/main/java/my/practice/FibonacciSeries.java
@@ -4,13 +4,14 @@ import java.util.HashMap;
 
 public class FibonacciSeries {
     public static long getNthTerm(int position) {
-        return fib(position, new HashMap<Integer, Long>());
+        return fib(position, new HashMap<>());
     }
 
     private static long fib(int n, HashMap<Integer, Long> memo) {
 
         if(memo.containsKey(n))
             return memo.get(n);
+
         if(n<=2)
             return 1;
 
