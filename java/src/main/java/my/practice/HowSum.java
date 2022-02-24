ad127131280780c359a5a6be6b16128e6eb0c4b4
unknown <krushnatkhawale@gmail.com>
2022-02-25 00:14:30 +0530

    Java: HowSum memoized, readme job badge added

--- java/src/main/java/my/practice/HowSum.java
+++ java/src/main/java/my/practice/HowSum.java
@@ -1,6 +1,7 @@
 package my.practice;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class HowSum {
 
@@ -9,21 +10,25 @@ public class HowSum {
     }
 
     public static int[] howSum(int targetSum, int[] numbers) {
+        return howSum(targetSum, numbers, new HashMap<>());
+    }
+    public static int[] howSum(int targetSum, int[] numbers, HashMap<Integer, int[]> memo) {
 
-        if(targetSum==0)
-            return new int[]{};
-        if(targetSum<0)
-            return null;
+        if(memo.containsKey(targetSum)) return memo.get(targetSum);
+        if(targetSum==0) return new int[]{};
+        if(targetSum<0) return null;
 
         for(int number : numbers){
-            final int[] newCombo = howSum(targetSum - number, numbers);
+            final int[] newCombo = howSum(targetSum - number, numbers, memo);
             if(newCombo!=null){
                 final int[] updatedCombo = Arrays.copyOf(newCombo, newCombo.length+1);
                 updatedCombo[newCombo.length] = number;
+                memo.put(targetSum, updatedCombo);
                 return updatedCombo;
             }
         }
 
+        memo.put(targetSum, null);
         return null;
     }
 }
